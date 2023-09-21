c700b19439e8e036d417760794fa36d16c54d9f0
bvv94 <bvdevasconcelos@gmail.com>
2023-09-21 09:15:35 -0400

    CreateGames implementado

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,9 +1,29 @@
 import {db} from "../database/database.connection.js"
 
+
 export async function getGames(req, res){
     try{
         const games = await db.query (`SELECT * FROM games;`);
+        res.send (games.rows)
     }catch(err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function createGames(req, res){
+    
+    const {name, image, stockTotal, pricePerDay} = req.body;
+    
+    try{
+        const games = await db.query (`SELECT * FROM games;`);
+        const exists = games.rows.find((g) => g.name === name)
+        if (exists) return res.status(409).send("Jogo já cadastrado")
+
+        await db.query (`INSERT INTO games (name, image, stockTotal, pricePerDay)
+                        VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+        res.sendStatus(201)
+    }
+    catch(err){
         res.status(500).send(err.message)
     }
 }
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,2 +1,11 @@
 import {Router} from "express";
+import { gameSchema } from "../schemas/games.schema.js";
+import { getGames, createGames } from "../controllers/games.controllers.js";
+import {validateSchema} from "../middlewares/validateSchema.middleware.js"
 
+const gamesRouter = Router();
+
+gamesRouter.get("/games", getGames);
+gamesRouter.post ("/games", validateSchema(gameSchema),createGames)
+
+export default gamesRouter;
